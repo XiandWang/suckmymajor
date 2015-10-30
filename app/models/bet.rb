dcd527e5ec320ba7e1e2509ff9eac5cd6ce91c30
Xiandong Wang <saburowang23@gmail.com>
2015-10-30 14:51:44 -0400

    delete picture from bet model

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,13 +1,11 @@
 class Bet < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc)}
-  mount_uploader :picture, PictureUploader
 
   validates :user_id, presence: true
   validates :status, presence: true, length: {maximum: 140}, inclusion: { in: ["studying", "procrastinating"] }
   validates :lying, inclusion: {in: [true, false]}
-  validates :proof, presence: true, allow_blank: true
-  validate :picture_size
+  
 
   acts_as_votable
 
@@ -33,11 +31,5 @@ class Bet < ActiveRecord::Base
     end
   end
 
-  private
 
-    def picture_size
-      if picture.size > 5.megabytes
-      	errors.add(:picture, "Picture's size is too big. Should be less than 5MB")
-      end
-    end
 end
