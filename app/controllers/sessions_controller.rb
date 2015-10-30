7a73c4600f85288050ebab9ff8d05a94fab0c743
Xiandong Wang <saburowang23@gmail.com>
2015-10-30 14:38:52 -0400

    delete sending email from sessions and users controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,9 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-      if user.activated?
-  		  log_in user
-  		  params[:session][:remeber_me] == '1' ? remember(user) : forget(user)
-  		  redirect_back_or root_url
-      else 
-        msg = "Accont Not Activated. "
-        msg += "Check you email for the activation link"
-        flash[:warning] = msg
-        redirect_to root_url
-      end 
+      log_in user
+  		params[:session][:remeber_me] == '1' ? remember(user) : forget(user)
+  		redirect_back_or root_url
   	else
   	  flash.now[:danger] = 'Invalid email/password combination.'
   	  render 'new'
