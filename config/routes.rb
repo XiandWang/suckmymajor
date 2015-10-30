0914897e996ef599e1272fc79be9af46be990ff6
Xiandong Wang <saburowang23@gmail.com>
2015-10-30 14:41:43 -0400

    delete activation controller

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,6 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   
   resources :bets, only: [:create, :destroy, :show] do
